dacb3bfbabcde84df9b34750e2cb2fc5e365ed9f
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-17 17:44:45 +0100

    Segundo commit;

--- Proyecto2/src/main/Main.java
+++ Proyecto2/src/main/Main.java
@@ -1,8 +1,9 @@
 package main;
 
 public class Main {
-	
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Mensaje1 rama1");
+        System.out.println("Mensaje2 rama1");
     }
 }
